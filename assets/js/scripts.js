d72fbb60baba2dd791dbcd413cbd897db316c841
Lucas Dias <lucasairies@gmail.com>
2025-09-18 14:12:08 -0300

    correção js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,41 @@
+// Elementos do DOM
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
 
-function changeTheme(){
+// Função para alternar tema
+function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
+  rootHtml.setAttribute("data-theme", currentTheme === "dark" ? "light" : "dark");
 
-  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
-
-  toggleTheme.classList.toggle("bi-sun")
-  toggleTheme.classList.toggle("bi-moon-stars")
+  if (toggleTheme) {
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+  }
 }
 
-toggleTheme.addEventListener("click", changeTheme);
+// Adiciona evento de click no toggleTheme, se existir
+if (toggleTheme) {
+  toggleTheme.addEventListener("click", changeTheme);
+}
 
-accordionHeaders.forEach(header => {
-  header.addEventListener("click", () => {
-    const accordionItem = header.parentElement;
-    const accordionActive = accordionItem.classList.contains("active");
+// Accordion funcional
+if (accordionHeaders.length > 0) {
+  accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+      const accordionItem = header.parentElement;
+      accordionItem.classList.toggle("active");
+    });
+  });
+}
 
-    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-  })
-})
-
-menuLinks.forEach(item => {
-  item.addEventListener("click", () => {
-    menuLinks.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  })
-})
+// Menu links ativos
+if (menuLinks.length > 0) {
+  menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+      menuLinks.forEach(i => i.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+}
